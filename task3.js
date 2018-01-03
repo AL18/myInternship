efed594055909d8d36112b8a439f0150465a3659
Ivan Chestakov <ivan.chestakov@globepayroll.com>
2018-01-03 12:01:19 +0200

    task 3 added recursion

--- task3.js
+++ task3.js
@@ -1,9 +1,20 @@
-function findFactorial(a) {
-    let factorial=1;
-    for (let i=1;i<=a;i++) {
-        factorial *= i;
+// function findFactorial(a) {
+//     let factorial=1;
+//     for (let i=1;i<=a;i++) {
+//         factorial *= i;
+//     }
+//     return factorial;
+// }
+//
+// console.log(findFactorial(4));
+let factorial = 1;
+
+function recursionFactorial(a) {
+    if (a > 1) {
+        factorial *= a;
+        recursionFactorial(--a);
     }
     return factorial;
 }
 
-console.log(findFactorial(4));
+console.log(recursionFactorial(4));
