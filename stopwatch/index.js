3704b55a4bb3e2034096ebb33aeefa8427d0ea64
Ivan Chestakov <ivan.chestakov@globepayroll.com>
2018-01-09 17:59:52 +0200

    finished number stopwatch

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -2,10 +2,14 @@
 
 class Stopwatch {
 
-    startTime = 0;
-    lapTime = 0;
+    constructor() {
+        this.startTime = 0;
+        this.lapTime = 0;
 
-    now = function() {
+
+    }
+
+    now () {
         return(new Date()).getTime();
     }
 
@@ -34,6 +38,12 @@ class Stopwatch {
 let x = new Stopwatch();
 let $time;
 let clocktimer;
+let flag = true;
+let $start = document.getElementById('start');
+let split_output = document.getElementById('split_output');
+let $split = document.getElementById('split');
+
+
 
 function pad(num, size) {
     var s = "0000" + num;
@@ -43,5 +53,63 @@ function pad(num, size) {
 function formatTime(time) {
     let h = m = s = ms = 0;
     let newTime = '';
-    
+
+    h = Math.floor( time / ( 60*60*1000 ) );
+    time = time % (60*60*1000);
+    m =  Math.floor( time / ( 60*1000 ) );
+    time = time % (60 * 1000 );
+    s = Math.floor( time / 1000 );
+    ms = time % 1000;
+
+    newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+    return newTime;
+}
+
+function show() {
+    $time = document.getElementById('time');
+    update;
+}
+
+function update() {
+    $time.innerHTML = formatTime(x.time())
+}
+
+function start() {
+    flag = false;
+    $start.value = 'Pause';
+    clocktimer = setInterval(update, 1);
+    $split.disabled = false;
+    x.start();
+}
+
+function stop() {
+    flag = true;
+    $start.value = ' Start ';
+    x.stop();
+    clearInterval(clocktimer);
+    $split.disabled = true;
+}
+
+function reset() {
+    stop();
+    x.reset();
+    update();
+    split_output.innerHTML = '';
+    $split.disabled = true;
+}
+
+function check() {
+    flag ?  start() : stop();
+}
+
+function split() {
+
+    if(split_output.children.length > 3) {
+        split_output.lastChild.innerText = 'Lap: ' + formatTime(x.time());
+    }
+    else {
+        let div = document.createElement('div');
+        div.innerText = 'Lap: ' + formatTime(x.time());
+        split_output.appendChild(div);
+    }
 }
