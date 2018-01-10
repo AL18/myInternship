a75ab462deae2785c7635e2651a64be506cf30bf
Ivan Chestakov <ivan.chestakov@globepayroll.com>
2018-01-10 15:29:14 +0200

    made first dial for stopwatch

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -1,4 +1,5 @@
 //'use strict'
+displayDial();
 
 class Stopwatch {
 
@@ -46,7 +47,7 @@ let $split = document.getElementById('split');
 
 
 function pad(num, size) {
-    var s = "0000" + num;
+    let s = "0000" + num;
     return s.substr(s.length - size);
 }
 
@@ -61,7 +62,9 @@ function formatTime(time) {
     s = Math.floor( time / 1000 );
     ms = time % 1000;
 
+    displayDial(m,s);
     newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+
     return newTime;
 }
 
@@ -72,6 +75,7 @@ function show() {
 
 function update() {
     $time.innerHTML = formatTime(x.time())
+    // displayDial();
 }
 
 function start() {
@@ -79,6 +83,7 @@ function start() {
     $start.value = 'Pause';
     clocktimer = setInterval(update, 1);
     $split.disabled = false;
+
     x.start();
 }
 
@@ -113,3 +118,108 @@ function split() {
         split_output.appendChild(div);
     }
 }
+
+function displayDial(min, sec) {
+    let canvasHTML = document.getElementById('dial');
+    let contextHTML = canvasHTML.getContext('2d');
+    contextHTML.strokeRect(0, 0, canvasHTML.width, canvasHTML.height);
+
+// Расчет координат центра и радиуса
+    let radiusClock = canvasHTML.width / 2 - 10;
+    let xCenterClock = canvasHTML.width / 2;
+    let yCenterClock = canvasHTML.height / 2;
+
+// Очистка экрана
+    contextHTML.fillStyle = '#ffffff';
+    contextHTML.fillRect(0, 0, canvasHTML.width, canvasHTML.height);
+
+// Kонтур часов
+    contextHTML.stokeStyle = '#000000';
+    contextHTML.lineWidth = 1;
+    contextHTML.beginPath();
+    contextHTML.arc(xCenterClock, yCenterClock, radiusClock, 0, 2 * Math.PI, true);
+    contextHTML.moveTo(xCenterClock, yCenterClock);
+    contextHTML.stroke();
+    contextHTML.closePath();
+
+// Рисочки часов
+    let radiusNum = radiusClock - 10; // Радиус расположения рисочек
+    let radiusPoint;
+    for ( let tm = 0; tm < 60; tm++ ) {
+        contextHTML.beginPath();
+        if (tm % 5 == 0) {
+            radiusPoint = 5;
+        } // Для выделения часовых рисочек
+        else {
+            radiusPoint = 2;
+        }
+
+        let xPointM = xCenterClock + radiusNum * Math.cos(-6 * tm * ( Math.PI / 180 ) + Math.PI / 2);
+        let yPointM = yCenterClock - radiusNum * Math.sin(-6 * tm * ( Math.PI / 180 ) + Math.PI / 2);
+
+        contextHTML.arc(xPointM, yPointM, radiusPoint, 0, 2 * Math.PI, true);
+        contextHTML.stroke();
+        contextHTML.closePath();
+    }
+
+// Оцифровка циферблата часов
+    for ( let th = 1; th <= 12; th++ ) {
+        contextHTML.beginPath();
+        contextHTML.font = 'bold 25px sans-serif';
+
+        let xText = xCenterClock + (radiusNum - 30) * Math.cos( -30 * th * ( Math.PI / 180 ) + Math.PI/2);
+        let yText = xCenterClock - (radiusNum - 30) * Math.sin( -30 * th * ( Math.PI / 180 ) + Math.PI/2);
+
+        if ( th <= 9 ) {
+            contextHTML.strokeText(th*5, xText - 12, yText + 10);
+        } else {
+            contextHTML.strokeText(th*5, xText - 18, yText + 10);
+        }
+    }
+    contextHTML.stroke();
+    contextHTML.closePath();
+
+
+// Рисуем стрелки
+    let lengthSeconds = radiusNum - 10;
+    let lengthMinutes = radiusNum - 15;
+    let d = new Date();
+    let t_sec = 6 * sec;
+    let t_min = 6 * ( min + ( 1/60 ) * sec );
+
+
+// Рисуем секунды
+    contextHTML.beginPath();
+    contextHTML.strokeStyle = '#FF0000';
+    contextHTML.moveTo(xCenterClock, yCenterClock);
+    contextHTML.lineTo( xCenterClock + lengthSeconds * Math.cos( Math.PI / 2 - t_sec * (Math.PI / 180 ) ),
+        yCenterClock - lengthSeconds * Math.sin( Math.PI / 2 - t_sec * ( Math.PI / 180 ) ) );
+    contextHTML.stroke();
+    contextHTML.closePath();
+
+// Рисуем минуты
+    contextHTML.beginPath();
+    contextHTML.strokeStyle = '#000000';
+    contextHTML.lineWidth = 3;
+    contextHTML.moveTo(xCenterClock, yCenterClock);
+    contextHTML.lineTo(xCenterClock + lengthMinutes * Math.cos (Math.PI / 2 - t_min * ( Math.PI / 180 ) ),
+    yCenterClock - lengthMinutes * Math.sin( Math.PI / 2 - t_min * ( Math.PI/180 ) ) );
+    contextHTML.stroke();
+    contextHTML.closePath();
+
+// Рисуем центр часов
+    contextHTML.beginPath();
+    contextHTML.strokeStyle = '#000000';
+    contextHTML.fillStyle = '#ffffff';
+    contextHTML.lineWidth = 3;
+    contextHTML.arc(xCenterClock, yCenterClock, 5, 0, 2 * Math.PI, true);
+    contextHTML.stroke();
+    contextHTML.fill()
+    contextHTML.closePath();
+
+
+    return;
+}
+
+
+
