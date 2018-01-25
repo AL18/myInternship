fec1bc89a4ae86017ba4b30272e075dda165c0f9
al18 <alexander22111998@gmail.com>
2018-01-25 18:17:55 +0200

    added 10.1

---
+++ marijnhaverbeke tasks/10. Symbols/1-Function compose.js
@@ -0,0 +1,16 @@
+// Symbols.1:
+// Function compose
+//
+// Define a symbol compose, and add it to Function.prototype,
+// with a method that composes the this function with the method's argument,
+// returning a new function that applies both composed functions,
+// one after the other, to its argument, and returns the result.
+let compose = Symbol('compose');
+
+Function.prototype[compose] = function(func) {
+    return  a => { return this(func(a)) }
+};
+
+let roundedAbs = Math.round[compose](Math.abs)
+console.log(roundedAbs(-5.5))
+// → 6
